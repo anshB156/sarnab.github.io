7a04c0eea1e789b684b25caee8c6a2c7e218fb98
Sarnab Bhattacharjee <bansh8561@gmail.com>
2025-04-24 01:04:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// script.js
 window.addEventListener('load', function() {
     setTimeout(function() {
       document.getElementById('loader').style.display = 'none';
       document.getElementById('content').style.display = 'block';
-    }, 6000); // 6000 milliseconds = 6 seconds
+    }, 6000); 
   });
